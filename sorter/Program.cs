5130f388b974cc3c14ea7ec5d00af054c6d7c376
Gardner Sullivan <gardnersullivan9@gmail.com>
2025-09-08 18:50:47 -0600

    Init

---
+++ sorter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace fileSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"sorting {string.Join(',',args)}");
+            if (args.Any(a => a == "--photos"))
+            {
+                Console.WriteLine("would you like to proceed Y/N");
+                var uinput = Console.ReadLine();
+                if (uinput == "Y")
+                {
+                    Console.WriteLine("Sorting your files");
+                }
+                else
+                {
+                    Console.WriteLine("See you again!");
+                }
+                
+            }
+        }
+    }
+}
+
